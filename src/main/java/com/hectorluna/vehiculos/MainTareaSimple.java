c8e16ea1860eec49fe51b91682e415afe3f92e3c
lunhect <hlunamoreno17@gmail.com>
2025-12-05 13:04:51 +0100

    first commit

---
+++ src/main/java/com/hectorluna/vehiculos/MainTareaSimple.java
@@ -0,0 +1,19 @@
+package com.hectorluna.vehiculos;
+
+public class MainTareaSimple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TareaSimple objeto1 = new TareaSimple("hola", false);
+	   
+		TareaSimple objeto2 = new TareaSimple("otra tarea más", true);
+	
+	    objeto2.mostrarTarea();
+	    objeto1.mostrarTarea();
+	    
+	 
+	    
+	    
+	}
+
+}
