e7f4adc18aed17e607a5da9a5ada94010faaf4d2
lunhect <130898696+lunhect@users.noreply.github.com>
2025-12-17 13:41:21 +0100

    Update GeneradorPerfiles.java

--- src/main/java/com/hectorlunamoreno/poo/redsocial/GeneradorPerfiles.java
+++ src/main/java/com/hectorlunamoreno/poo/redsocial/GeneradorPerfiles.java
@@ -3,14 +3,25 @@ package com.hectorlunamoreno.poo.redsocial;
 public class GeneradorPerfiles {
 	 
 	public static PerfilRedSocial testProfileGenerator(String userName) {
-		return new PerfilRedSocial(userName, userName, "¡Hola a todos!", "Madrid");
+		return new PerfilRedSocial(userName, userName, "¡Hola a todos!", "Amsterdam");
 	}
 	
-	public static PerfilRedSocial testProfilePostIncludedGenerator(String username, int postsNumber) {
-		PerfilRedSocial p = testProfileGenerator(username);
-		for (int i = 1; i <= postsNumber; i++) {
-			p.newPost("Publicacion " + i);
-		}
-		return p;
+	public static PerfilRedSocial testPerfilTestGenerator(String username, int postsNumber) {
+	    PerfilRedSocial p = testProfileGenerator(username);
+	    for (int i = 1; i <= postsNumber; i++) {
+	        p.crearPublicacion("Numero post " + i); 
+	    }
+	    return p;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
